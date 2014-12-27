bd29f99655fbfa849d700eb9131b27b8af401d78
Jakob Buis <jpabuis@gmail.com>
2014-12-27 21:07:23 +0100

    added dummy key and button handlers, commented for not working

--- javascripts/mjs.js
+++ javascripts/mjs.js
@@ -1,8 +1,9 @@
 // Start behaviour when DOM is ready
 document.addEventListener('DOMContentLoaded', function(){
 
-    // Initiate music master control
-    mjs = new MusicMaster('http://www.delftelectronics.nl/musicmaster/');
+    // // Initiate music master control
+    // mjs = new MusicMaster('http://www.delftelectronics.nl/musicmaster/');
+    // mjs.players.listMjs();
 
     // Testing code for drawing the progress unit
     var song = document.querySelector('.song');
@@ -12,4 +13,56 @@ document.addEventListener('DOMContentLoaded', function(){
         i++;
     }, 10);
 
+    /*// Global event handlers (play, pause, etc)
+    document.getElementById('control-clear').addEventListener('click', mjs.clear);
+    document.getElementById('control-search').addEventListener('click', mjs.search);
+    document.getElementById('control-shuffle').addEventListener('click', mjs.shuffle);
+    document.getElementById('control-previous').addEventListener('click', mjs.previous);
+    document.getElementById('control-stop').addEventListener('click', mjs.stop);
+    document.getElementById('control-play').addEventListener('click', mjs.play);
+    document.getElementById('control-forward').addEventListener('click', mjs.forward);
+
+    // Keyboard shortcuts
+    document.addEventListener('keyup', function(event){
+        var el;
+        switch (event.keyCode)
+        {
+            case 67:    // C - clear
+            {
+                el = document.getElementById('control-clear'); break;
+            }
+            case 70:    // F - search (find)
+            {
+                el = document.getElementById('control-search'); break;
+            }
+            case 83:    // S - stop
+            {
+                el = document.getElementById('control-stop'); break;
+            }
+            case 37:    // Left - previous song
+            {
+                el = document.getElementById('control-previous'); break;
+            }
+            case 72:    // H - stop (halt)
+            {
+                el = document.getElementById('control-stop'); break;
+            }
+            case 32:    // Space - play/pause
+            {
+                el = document.getElementById('control-play'); break;
+            }
+            case 39:    // Right - forward song
+            {
+                el = document.getElementById('control-forward'); break;
+            }
+            default:
+            {
+                return; // KeyCode not caught, proceed normally
+            }
+        }
+        event.preventDefault();
+        event = document.createEvent('HTMLEvents');
+        event.initEvent('click', true, false);
+        el.dispatchEvent(event);
+    });*/
 });
